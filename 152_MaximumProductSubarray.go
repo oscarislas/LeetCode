e0ca5be6d83c8dad2d4656e2d0a5399e984fdf9d
Oscar Islas <islas@uber.com>
2020-06-01 17:14:13 -0700

    152

---
+++ 152_MaximumProductSubarray.go
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/maximum-product-subarray/
+//152. Maximum Product Subarray
+
+// Given an integer array nums, find the contiguous subarray within an array
+// (containing at least one number) which has the largest product.
+// Example 1:
+// Input: [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+
+package main
+
+func maxProduct(nums []int) int {
+	return 0
+}
